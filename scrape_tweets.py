64ec7527f617c558e1f2770531f98cea92d55565
Daanish Ali <daanishali921@gmail.com>
2022-01-13 22:08:46 -0500

    scrapeTweets function to scrape tweets

---
+++ scrape_tweets.py
@@ -0,0 +1,40 @@
+import snscrape.modules.twitter as sntwitter
+import os
+import pandas as pd
+
+
+
+def scrapeTweets(start, stop, keyword, directory, tweet_limit=1):
+    if not os.path.exists(directory):  # Creates directory in current directory if doesn't already exist
+        os.mkdir(directory)
+
+    file_path = os.path.join(directory, f'keyword:{keyword}__start:{start}_end:{stop}__limit:{tweet_limit}.csv')
+
+    tweet_list = []
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'{keyword} since:{start} until:{stop}').get_items()):
+        if i > tweet_limit:
+            break
+        tweet_list.append([tweet.date,  # Appending all tweet data into a list of list
+                           tweet.id,
+                           tweet.content,
+                           tweet.user.username,
+                           tweet.user.followersCount,
+                           tweet.hashtags,
+                           tweet.cashtags,
+                           tweet.lang])
+
+    df_tweets = pd.DataFrame(tweet_list, columns=['Datetime',  # Creating df of tweet data
+                                                  'Tweet Id',
+                                                  'Text',
+                                                  'Username',
+                                                  'Followers Count',
+                                                  'Hashtags',
+                                                  'Cashtags',
+                                                  'Language'])
+
+    df_tweets.to_csv(file_path, index=False)  # Writing df_tweets into new csv file
+
+    if os.path.isfile(file_path) == True:
+        return print(f'Successfully saved DataFrame to {file_path}')
+    else:
+        return print('DataFrame not saved -- possible error has occurred.')
