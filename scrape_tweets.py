6a4e87d60d30052feb24acba6b9fc0335b5749fe
Daanish Ali <daanishali921@gmail.com>
2022-01-20 19:44:58 -0500

    added more features to scrape to dataframe

--- scrape_tweets.py
+++ scrape_tweets.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import date
 
 
-def scrapeTweets(start_date, end_date, keyword, current_dir, tweet_limit=1, iteration=1):
+def scrapeTweets(start_date, end_date, keyword, current_dir, tweet_limit=1, iteration=0):
     if not os.path.exists(current_dir):  # Creates directory in current directory if doesn't already exist
         os.mkdir(current_dir)
 
@@ -22,7 +22,10 @@ def scrapeTweets(start_date, end_date, keyword, current_dir, tweet_limit=1, iter
                            tweet.user.followersCount,
                            tweet.hashtags,
                            tweet.cashtags,
-                           tweet.lang])
+                           tweet.lang,
+                           tweet.retweetCount,
+                           tweet.likeCount,
+                           tweet.replyCount])
 
     df_tweets = pd.DataFrame(tweet_list, columns=['Datetime',  # Creating df of tweet data
                                                   'Tweet Id',
@@ -31,15 +34,18 @@ def scrapeTweets(start_date, end_date, keyword, current_dir, tweet_limit=1, iter
                                                   'Followers Count',
                                                   'Hashtags',
                                                   'Cashtags',
-                                                  'Language'])
+                                                  'Language',
+                                                  'Retweet Count',
+                                                  'Likes Count',
+                                                  'Reply Count'])
 
-    for i in range(1, 1001):
+    for i in range(1001):
         if not os.path.isfile(file_path):
             df_tweets.to_csv(file_path, index=False)
             break
         else:
             file_path = os.path.join(current_dir,
-                                     f'keyword:{keyword}__start:{start_date}_end:{end_date}__limit:{tweet_limit}__iter:{iteration + 1}.csv')
+                                     f'keyword:{keyword}__start:{start_date}_end:{end_date}__limit:{tweet_limit}__iter:{i + 1}.csv')
 
     if os.path.isfile(file_path):
         return print(f'Successfully saved DataFrame to {file_path}')
