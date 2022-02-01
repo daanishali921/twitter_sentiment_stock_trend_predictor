abb14b63cfd94aba79a6b0921881ccf9e73bf4c5
Daanish Ali <60590897+daanishali921@users.noreply.github.com>
2022-02-01 16:00:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 mkdir -p ~/.streamlit/
-echo "[theme]
-base="dark"
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[theme]\n\
+base='dark'\n\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
